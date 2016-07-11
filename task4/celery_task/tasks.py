a80c4f856dabc3feee3437aecf987fd442bcb721
taraskl <taras.kliuchnyk@gmail.com>
2016-07-11 18:19:41 +0300

    change tasks

--- task4/celery_task/tasks.py
+++ task4/celery_task/tasks.py
@@ -1,21 +1,3 @@
-# from __future__ import absolute_import
-
-# from celery import shared_task
-
-
-# @shared_task
-# def add(x, y):
-#     return x + y
-
-
-# @shared_task
-# def mul(x, y):
-#     return x * y
-
-
-# @shared_task
-# def xsum(numbers):
-#     return sum(numbers)
 from celery.task import periodic_task
 from datetime import timedelta
 from api import all_hotels
@@ -35,16 +17,5 @@ def add(x, y):
 	a = x + y
 	print 'result {0}'.format(a)	
 
-# def delay(self, *args, **kwargs):
-#         """Star argument version of :meth:`apply_async`.
-
-#         Does not support the extra options enabled by :meth:`apply_async`.
-
-#         :param \*args: positional arguments passed on to the task.
-#         :param \*\*kwargs: keyword arguments passed on to the task.
-
-#         :returns :class:`celery.result.AsyncResult`:
-
-#         """
-#     return self.apply_async(args, kwargs)			    
+		    
 
